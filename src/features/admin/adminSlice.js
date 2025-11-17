cf7a9e2c120fd77d4138d7345c4711bd9c9d5744
Fazil <fazilmm860@gmail.com>
2025-11-17 12:43:22 +0530

    updated

--- src/features/admin/adminSlice.js
+++ src/features/admin/adminSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import attendanceService from './attendanceService';
+import attendanceService from '../attendance/attendanceService';
 
 // Initial state
 const initialState = {
